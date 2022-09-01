af7cb1f54bfe18fc2cb1de2ee9ae6e1067de77b6
Raghav <90678701+raghavhirani07@users.noreply.github.com>
2022-09-01 11:31:53 +0800

    Add files via upload

---
+++ partials/_footer.php
@@ -0,0 +1,6 @@
+<?php echo'<div class="container-fluid bg-dark text-light " >
+
+    <p class="text-center p-2 ">   Copywrite@ 2022</p>
+    <p class="text-center p-2 ">   Copywrite@ 2022</p>
+</div>';
+?>
